33d6fcd565a12f635cb55204173e5ec6c5b96360
Yvazz <ziroustyle@gmail.com>
2019-12-08 20:25:18 +0100

    added input options on console

--- main.py
+++ main.py
@@ -9,13 +9,14 @@ DRIVER_PATH = os.path.abspath("./drivers/windows/")
 # DRIVER_PATH = os.path.abspath("./drivers/macos/")
 
 
-def run_tinder():
+def run_tinder(sequence_mode: str = str(1)):
     URL: str = 'https://tinder.com/app/recs'
     tinder = Tinder(URL, DRIVER_PATH, 'chrome')
-    tinder.run_like_sequence_by_phone('611400206', 50000)
 
-    # Uncomment for using facebook account
-    # tinder.run_sequence_by_facebook(50000)
+    if sequence_mode == str(1):
+        tinder.run_like_sequence_by_phone('611400206', 50000)
+    elif sequence_mode == str(2):
+        tinder.run_sequence_by_facebook(50000)
 
 
 def run_lovoo():
@@ -25,5 +26,19 @@ def run_lovoo():
 
 
 if __name__ == '__main__':
-    run_tinder()
-    # run_lovoo()
+    print("===================")
+    print("1) Tinder")
+    print("2) Lovoo")
+    print("===================")
+    option = input()
+
+    if option == str(1):
+        print("======TINDER=======")
+        print("1) By phone number")
+        print("2) By facebook page")
+        print("===================")
+        mode = input()
+        run_tinder(mode)
+
+    elif option == str(2):
+        run_lovoo()
